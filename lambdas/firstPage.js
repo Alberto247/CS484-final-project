9b02243dcb26999204962567acd32be3370543b3
Davide Porello <davide.porello29@gmail.com>
2022-12-01 23:55:01 -0600

    api fixes

--- lambdas/firstPage.js
+++ lambdas/firstPage.js
@@ -39,43 +39,47 @@ export const handler = async (event, context, callback) => {
 	}
 	
 	let ret = undefined;
-	
-	//sneaks API
-	sneaks.getMostPopular(7, function(err, products) { 
-		if(products) {
-			ret = products;
+
+	try {
+		//sneaks API
+		sneaks.getMostPopular(7, function(err, products) { 
+			if(products) {
+				ret = products;
+			}
+			else 
+				ret = [];
+		});
+
+		while(ret === undefined){
+			await new Promise(r => setTimeout(r, 100));
 		}
-		else 
-			ret = [];
-	});
 
-	while(ret === undefined){
-		await new Promise(r => setTimeout(r, 100));
-	}
-
-	//klekt
-	const data = await (await fetch("https://www.klekt.com/brands")).text();
-	let api_path = data.split('/_buildManifest.js" defer=""></script>')[0].split('<script src="/_next/static/');
-	api_path = api_path[api_path.length - 1]
-	let api = "https://www.klekt.com/_next/data/"+api_path+"/eu/list.json?category=brands&categories=brands&page=1"
-	let unparsed = await (await fetch(api)).json();
-	if(unparsed) {
-		unparsed = unparsed["pageProps"]["plpData"]["data"]["search"]["items"];
-		unparsed.forEach((e) => {
-			let newElem = {"_id":e.productId, "shoeName":e.productName, "brand": e.brandNames?.length>0?e.brandNames[0]:"", "thumbnail":e.productAsset?.preview, "description":e.description, "lowestResellPrice":{"klekt":e.priceWithTax.min/100}, "resellLinks":{"klekt":"https://www.klekt.com/product/"+e.slug}};
-			ret.push(newElem);
-		});
-	}
-	api = "https://www.klekt.com/_next/data/"+api_path+"/eu/list.json?category=brands&categories=brands&page=2"
-	unparsed = await (await fetch(api)).json();
-	if(unparsed) {
-		unparsed = unparsed["pageProps"]["plpData"]["data"]["search"]["items"];
-		unparsed.forEach((e) => {
-			let newElem = {"_id":e.productId, "shoeName":e.productName, "brand": e.brandNames?.length>0?e.brandNames[0]:"", "thumbnail":e.productAsset?.preview, "description":e.description, "lowestResellPrice":{"klekt":e.priceWithTax.min/100}, "resellLinks":{"klekt":"https://www.klekt.com/product/"+e.slug}};
-			if(ret.length < 30)
+		//klekt
+		const data = await (await fetch("https://www.klekt.com/brands")).text();
+		let api_path = data.split('/_buildManifest.js" defer=""></script>')[0].split('<script src="/_next/static/');
+		api_path = api_path[api_path.length - 1]
+		let api = "https://www.klekt.com/_next/data/"+api_path+"/eu/list.json?category=brands&categories=brands&page=1"
+		let unparsed = await (await fetch(api)).json();
+		if(unparsed) {
+			unparsed = unparsed["pageProps"]["plpData"]["data"]["search"]["items"];
+			unparsed.forEach((e) => {
+				let newElem = {"_id":e.productId, "shoeName":e.productName, "brand": e.brandNames?.length>0?e.brandNames[0]:"", "thumbnail":e.productAsset?.preview, "description":e.description, "lowestResellPrice":{"klekt":e.priceWithTax.min/100}, "resellLinks":{"klekt":"https://www.klekt.com/product/"+e.slug}};
 				ret.push(newElem);
-		});
-	}
+			});
+		}
+		api = "https://www.klekt.com/_next/data/"+api_path+"/eu/list.json?category=brands&categories=brands&page=2"
+		unparsed = await (await fetch(api)).json();
+		if(unparsed) {
+			unparsed = unparsed["pageProps"]["plpData"]["data"]["search"]["items"];
+			unparsed.forEach((e) => {
+				let newElem = {"_id":e.productId, "shoeName":e.productName, "brand": e.brandNames?.length>0?e.brandNames[0]:"", "thumbnail":e.productAsset?.preview, "description":e.description, "lowestResellPrice":{"klekt":e.priceWithTax.min/100}, "resellLinks":{"klekt":"https://www.klekt.com/product/"+e.slug}};
+				if(ret.length < 30)
+					ret.push(newElem);
+			});
+		}
+	} catch (err) {
+        console.error("Error in handle shoe", err);
+    }
 
 	const newRet=[];
 	for(let e of ret){
