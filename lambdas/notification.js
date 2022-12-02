5cfd21ee677b331693591847b7735619aa0103a2
Alberto247 <albertocarboneri99@gmail.com>
2022-12-01 18:12:45 -0600

    fixed potential bug

--- lambdas/notification.js
+++ lambdas/notification.js
@@ -47,44 +47,55 @@ const sendMail = async (options) => {
 };
 
 const checkPrice = async (shoe, users) => {
-    const response = await fetch('https://fluffy-dusk-8cf61e.netlify.app/.netlify/functions/search?page=1&search=' + encodeURIComponent(shoe.productInfo.shoeName));
-    const product = await response.json();
-    if (response.ok) {
-        let res = product.products.filter((e) => { return e.shoeName === shoe.productInfo.shoeName })[0];
-        const x = Object.keys(res.lowestResellPrice);
-        for (let j = 0; j < x.length; j++){
-            if (res.lowestResellPrice[x[j]] <= shoe.threshold) {
-                const { _, err } = await supabase.from('preferred_shoes').update({ threshold: null }).eq('id', shoe.id);
-                //const err=null;
-                if (err) {
-                    console.log(err);
-                    throw err;
-                }
+    try {
+        const response = await fetch('https://fluffy-dusk-8cf61e.netlify.app/.netlify/functions/search?page=1&search=' + encodeURIComponent(shoe.productInfo.shoeName));
+        const product = await response.json();
+        if (response.ok) {
+            let res = product.products.filter((e) => { return e.shoeName === shoe.productInfo.shoeName })[0];
+            const x = Object.keys(res.lowestResellPrice);
+            for (let j = 0; j < x.length; j++) {
+                if (res.lowestResellPrice[x[j]] <= shoe.threshold) {
+                    const { _, err } = await supabase.from('preferred_shoes').update({ threshold: null }).eq('id', shoe.id);
+                    //const err=null;
+                    if (err) {
+                        console.error(err);
+                        return;
+                    }
 
 
-                const email = users.filter((e) => e.id === shoe.user_id)[0].email;
-                const fileAttachments = [];
-                const uri = 'https://sneakerscanner.shop/#/view/' + encodeURIComponent(shoe.productInfo.shoeName);
-                const options = {
-                    to: email,
-                    cc: '',
-                    subject: shoe.productInfo.shoeName + '\'s price is low!',
-                    text: 'We have detected that ' + shoe.productInfo.shoeName + '\'s price is lower than what you asked us to watch for!',
-                    html: `<h1>Buy now your shoes!</h1>
+                    const email = users.filter((e) => e.id === shoe.user_id)[0].email;
+                    const fileAttachments = [];
+                    const uri = 'https://sneakerscanner.shop/#/view/' + encodeURIComponent(shoe.productInfo.shoeName);
+                    const options = {
+                        to: email,
+                        cc: '',
+                        subject: shoe.productInfo.shoeName + '\'s price is low!',
+                        text: 'We have detected that ' + shoe.productInfo.shoeName + '\'s price is lower than what you asked us to watch for!',
+                        html: `<h1>Buy now your shoes!</h1>
                             <p>We have detected that <a href=${uri}>${shoe.productInfo.shoeName}'\'s</a> price is lower than what you asked us to watch for!<br>
                             <img src="${shoe.productInfo.thumbnail}"></img></p>`,
 
-                    attachments: fileAttachments,
-                    textEncoding: 'base64',
-                    headers: [
-                        { key: 'X-Application-Developer', value: 'Pizza Pasta Mandolino' },
-                        { key: 'X-Application-Version', value: 'v1.0' },
-                    ],
-                };
-                console.log("Email sent to " + email);
-                const messageId = await sendMail(options);
-                break;
+                        attachments: fileAttachments,
+                        textEncoding: 'base64',
+                        headers: [
+                            { key: 'X-Application-Developer', value: 'Pizza Pasta Mandolino' },
+                            { key: 'X-Application-Version', value: 'v1.0' },
+                        ],
+                    };
+                    console.log("Email sent to " + email);
+                    const messageId = await sendMail(options);
+                    break;
+                }
             }
+        }else{
+            console.error(response);
+        }
+    } catch (err) {
+        console.error("Error in handle shoe", err)
+        try{
+            const { _, err } = await supabase.from('preferred_shoes').update({ threshold: null }).eq('id', shoe.id);
+        } catch(err2) {
+            console.log("Cannot set shoe back to null", err2);
         }
     }
 }
@@ -96,11 +107,11 @@ const checkThreshold = async () => {
         console.log(error);
         throw error;
     }
-    const updates=[]
-    for(let i =0;i<data.length;i++)
-        updates.push(supabase.from("preferred_shoes").update({ last_controlled_at: Math.floor(Date.now()/1000) }).eq('id', data[i].id));
+    const updates = []
+    for (let i = 0; i < data.length; i++)
+        updates.push(supabase.from("preferred_shoes").update({ last_controlled_at: Math.floor(Date.now() / 1000) }).eq('id', data[i].id));
 
-    console.log("Checking " + data.length+" shoes");
+    console.log("Checking " + data.length + " shoes");
     const res = await supabase.from('users').select();
     const users = res.data;
     const error2 = res.error;
@@ -108,12 +119,12 @@ const checkThreshold = async () => {
         console.log(error2);
         throw error2;
     }
-    const promises=[]
+    const promises = []
     for (let i = 0; i < data.length; i++) {
-        promises.push(checkPrice(data[i], users));
+        promises.push(checkPrice(data[i], users)); //The majority of the time is asyncronous waiting for fetch results. Avoiding waiting here speeds up everything by a lot.
     }
-    await Promise.all(promises);
-    await Promise.all(updates);
+    await Promise.all(promises); //And then we wait all fetches together
+    await Promise.all(updates); //This improves a little
 }
 
 const handler = async function (event, context) {
